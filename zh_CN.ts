fb91ad32a81ab91227b8c273d4c09e9e44994522
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-06-02 23:08:52 +0900

    多言語対応
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

---
+++ zh_CN.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>InputArea</name>
+    <message>
+        <location filename="kb_ui.py" line="44"/>
+        <source>Type here…</source>
+        <translation>輸入…</translation>
+    </message>
+</context>
+<context>
+    <name>EditorWindow</name>
+    <message>
+        <location filename="kb_ui.py" line="122"/>
+        <source>Reload</source>
+        <translation>重新載入</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="136"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="150"/>
+        <source>Go-To…</source>
+        <translation>跳轉到行…</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="163"/>
+        <source>Help</source>
+        <translation>幫助</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="172"/>
+        <source>The current incomplete word you&apos;ve inputted will be displayed at here.</source>
+        <translation>輸入中的單詞會顯示在此。</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="202"/>
+        <source>Leave window?
+
+Contents will not be saved.
+</source>
+        <translation>要关閉窗口嗎？
+
+未保存的內容可能丟失。
+</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="256"/>
+        <source>&lt;font color=&quot;red&quot;&gt;• Keyboard Disabled&lt;/font&gt;</source>
+        <translation>&lt;font color=&quot;red&quot;&gt;•鍵盤禁用中&lt;/font&gt;</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="289"/>
+        <source>&lt;Tab&gt;		Toggle keyboard ON/OFF
+&lt;Space&gt;		Transform words
+&lt;Enter&gt;		Commit word
+&lt;ESC&gt;		Trash word buffer
+
+&lt;{saveKeyRepr}&gt;		Save into disk
+&lt;{gotoKeyRepr}&gt;		Jump to line
+&lt;{reloadKeyRepr}&gt;		Read again from file
+&lt;{closeKeyRepr}&gt;		Close window
+
+(To type tiếng, say tieesng, not tieengs.)
+</source>
+        <translation>&lt;Tab&gt;		開啓／禁用鍵盤
+&lt;Space&gt;		轉換當前輸入到漢喃字
+&lt;Enter&gt;		上屏
+&lt;ESC&gt;		廢棄未上屏內容
+
+&lt;{saveKeyRepr}&gt;		寫出文件
+&lt;{gotoKeyRepr}&gt;		跳轉至行
+&lt;{reloadKeyRepr}&gt;		再次讀取文件
+&lt;{closeKeyRepr}&gt;		关閉窗口
+
+（要輸入tiếng，請按tieesng而不是tieengs。）
+</translation>
+    </message>
+    <message>
+        <location filename="kb_ui.py" line="319"/>
+        <source>Specify the destination line number to switch to:</source>
+        <translation>輸入要跳轉至的行号：</translation>
+    </message>
+</context>
+<context>
+    <name>ctx_end_program_key_bind</name>
+    <message>
+        <location filename="app.py" line="44"/>
+        <source>Exit application?
+
+Unsaved changes will be lost!
+</source>
+        <translation>要退出应用程序嗎？
+
+未保存的变更可能丟失！
+</translation>
+    </message>
+</context>
+<context>
+    <name>ctx_main</name>
+    <message>
+        <location filename="app.py" line="56"/>
+        <source>Nôm4VI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="app.py" line="60"/>
+        <source>Chữ Nôm workstation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="app.py" line="69"/>
+        <source>%s: Too many positional arguments</source>
+        <translation>%s: 参数過多</translation>
+    </message>
+</context>
+</TS>
